03903ce537dd7344ec76838837de74b6bd152d4c
BugHunter-X <askzenai@gmail.com>
2025-02-16 00:02:05 +0100

    Add server.js file

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.setHeader('Content-Type', 'application/json');
+  response.writeHead(200);
+  response.end(JSON.stringify({ message: 'Hello from Node.js!' }));
+};
+
+const server = http.createServer(requestListener);
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
